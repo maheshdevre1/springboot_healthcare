647eb3d825018759412cb50ad829fd5e318af311
maheshdevre1 <maheshdevre1997@gmail.com>
2026-05-09 22:30:00 +0530

    healthcare project

---
+++ src/test/java/com/mahesh/healthcare/HealthCareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mahesh.healthcare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthCareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
